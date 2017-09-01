15b4bd9f1979c1eed7ef54273a7e51d44e801b43
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-09-01 20:49:21 -0300

    Button component created

---
+++ src/components/Button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+
+
+const Button = () => {
+    const {buttonStyle, buttonTextStyle} = styles;
+    return (
+        <TouchableOpacity style={buttonStyle}>
+            <Text style={buttonTextStyle}>
+                I'm a Button
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+styles  = {
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007AFF',
+        marginLeft: 5,
+        marginRight: 5,
+    },
+
+    buttonTextStyle: {
+        alignSelf: 'center',
+        color: '#007AFF',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+};
+
+export default Button;
