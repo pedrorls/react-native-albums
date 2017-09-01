cc3081677b3e26ba60665e888caac9e634e6abef
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-09-01 20:05:11 -0300

    albumDetail component now shows info from albums

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,66 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+    const {title, artist, thumbnail_image, image} = album;
+    const {thumbnailStyle, headerTextStyle, thumbnailContainerStyle, albumNameStyle, imageStyle} = styles;
     return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                        style={thumbnailStyle} 
+                        source={{uri: thumbnail_image}}
+                    />
+                </View>
+                <View style={headerTextStyle}>
+                    <Text style={albumNameStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                    style={imageStyle}
+                    source={{uri: image}}
+                />
+            </CardSection>
         </Card>
 
     );
 
+};
+
+const styles = {
+    headerTextStyle:  {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+
+    albumNameStyle: {
+        fontSize: 18,
+    },
+
+    thumbnailStyle: {
+        height: 50,
+        width: 50,
+    },
+
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 10,
+        marginLeft: 10,
+    },
+
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null,
+
+    },
 };
 
 export default AlbumDetail;
