cad19aaf7484d1113d63f4d45962d17a399d847c
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-09-01 15:51:44 -0300

    list component created using class based component

--- index.android.js
+++ index.android.js
@@ -1,11 +1,15 @@
 // Importe library to help create a component
 import React from 'react';
-import {AppRegistry, Text} from 'react-native';
-import Header from './src/components/header';
+import {AppRegistry, View} from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a a component
 const App = () => (
+  <View>
     <Header headerText={'Albums'}/>
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
